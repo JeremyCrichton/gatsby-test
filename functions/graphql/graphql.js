33a40110adbd088d94f11e45bb0153d0ac558166
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-04 14:26:36 -0700

    update gql settings

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -54,4 +54,9 @@ const server = new ApolloServer({
   introspection: true,
 })
 
-exports.handler = server.createHandler()
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+})
