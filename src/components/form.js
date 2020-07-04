e9724351ce6bb22a83542cd37827be379aaaac63
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-03 20:27:35 -0700

    add netlify form

---
+++ src/components/form.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react"
+
+const Form = () => {
+  const [name, setName] = useState("")
+  const [level, setLevel] = useState("")
+  const [message, setmessage] = useState("This site is amazing...")
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      action="/thanks-page"
+    >
+      <label htmlFor="name">Your name: </label>
+      <p>
+        <input
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </p>
+      <label htmlFor="level">How awesome am I?</label>
+      <p>
+        <select
+          value={level}
+          onChange={e => setLevel(e.target.value)}
+          name="level"
+          id="level"
+        >
+          <option value="most">The most</option>
+          <option value="more">Even more than that</option>
+        </select>
+      </p>
+      <label htmlFor="message">Write a nice message to me: </label>
+      <p>
+        <textarea
+          name="message"
+          value={message}
+          onChange={e => setmessage(e.target.value)}
+        />
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  )
+}
+
+export default Form
---
+++ src/pages/thanks-page.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThanksPage = () => (
+  <Layout>
+    <SEO title="Thanks Page" />
+    <h1>Thanks!</h1>
+  </Layout>
+)
+
+export default ThanksPage
