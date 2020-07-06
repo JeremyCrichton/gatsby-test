982ba216a92eb13fc7fe6b08486aabd17424c4a9
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-06 12:44:36 -0700

    update auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Login from "../components/login"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>This is the Homepage</h1>
-    <Link to="/form-page/">Send us a message...</Link> <br />
-    <Link to="/todo-page/">Go to the todo page</Link> <br />
-    <Link to="/login-page/">Go to the good stuff...</Link> <br />
-  </Layout>
-)
+let netlifyIdentity
+if (typeof window !== "undefined") {
+  netlifyIdentity = require("netlify-identity-widget")
+}
+
+const IndexPage = () => {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>This is the Homepage</h1>
+      <Link to="/form-page/">Send us a message...</Link> <br />
+      <Link to="/todo-page/">Go to the todo page</Link> <br />
+      <Login />
+    </Layout>
+  )
+}
 
 export default IndexPage
