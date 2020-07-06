aedfc76c095faab45145a290631d7193e7f93ff0
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-06 13:15:46 -0700

    update auth settings

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,29 +7,13 @@ import {
   HttpLink,
 } from "@apollo/client"
 import fetch from "isomorphic-fetch"
-import { setContext } from "apollo-link-context"
-import netlifyIdentity from "netlify-identity-widget"
-
-const authLink = setContext((_, { headers }) => {
-  const user = netlifyIdentity.currentUser()
-  const token = user.token.access_token
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      Authorization: token ? `Bearer ${token}` : "",
-    },
-  }
-})
-
-const httpLink = new HttpLink({
-  fetch,
-  uri: "https://dummy-heads.netlify.app/.netlify/functions/graphql",
-})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: new HttpLink({
+    fetch,
+    uri: "https://dummy-heads.netlify.app/.netlify/functions/graphql",
+  }),
 })
 
 export const wrapRootElement = ({ element }) => {
